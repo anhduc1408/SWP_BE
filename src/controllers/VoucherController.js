2635fe5361cce7affdcd45a57ec1c5ccd274a24b
anhduc111 <154323330+anhduc111@users.noreply.github.com>
2025-02-08 14:15:04 +0700

    OrderCheckOut v1

--- src/controllers/VoucherController.js
+++ src/controllers/VoucherController.js
@@ -2,10 +2,14 @@ const VoucherService = require('../services/VoucherService');
 
 const Voucher = {
     getVoucherByCusID: async(req,res)=>{
-        const cusID = req.body.cusID;
+        try {
+            const cusID = req.body.cusID;
         const totalPrice = req.body.totalPrice;
         const result = await VoucherService.getVoucherByCusID(cusID,totalPrice);
-        res.json(result)
+        res.status(200).json(result)
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 
