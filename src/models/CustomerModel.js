d6f3a0f2f6fcb3385bdd95c665366ba1594ab1eb
dpnghia2902 <nghiadphe180494@fpt.edu.vn>
2025-02-09 12:32:35 +0700

    Xóa file không cần thiết

--- src/models/CustomerModel.js
+++
@@ -1,9 +0,0 @@
-const pool = require('../config/Database');
-
-const Customer = {
-    getCustomerByID:async(cusID)=>{
-        const result = await pool.query('select * from Customer where CustomerID = ?',[cusID]);
-        return result;
-    }
-}
-module.export = Customer;
