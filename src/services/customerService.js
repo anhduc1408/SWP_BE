71b722d433460716e0895ebd880b34c3b15b6d95
dpnghia2902 <nghiadphe180494@fpt.edu.vn>
2025-02-09 13:13:33 +0700

    xóa

--- src/services/customerService.js
+++
@@ -1,8 +0,0 @@
-const CustomerModels = require('../models/CustomerModel')
-
-const Customer = {
-    getCustomerByID: async(cusID)=>{
-        return await CustomerModels.getCustomerByID(cusID);
-    }
-}
-module.exports = Customer;
