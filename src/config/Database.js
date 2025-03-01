77b26215c7812d2030fbf673022226bb008a77bf
viet30072001 <vietddhe151061@fpt.edu.vn>
2025-03-01 12:58:30 +0700

    fix lại get shop id

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host:'34.56.84.253',
-    user:'vietdd',
-    password:'123321',
+    host:'localhost',
+    user:'root',
+    password:'root',
     database:'G5_Customer',
     port :3306,
     connectTimeout: 50000
--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -1,5 +1,4 @@
 const pool = require("../config/Database");
-const { checkUserCanComment } = require("../services/ProductService");
 const Products = {
   getAllProductsNew: async (option, type) => {
     if (option === "Tất Cả") {
@@ -177,7 +176,7 @@ const Products = {
     }
 
     const queryDocs = `
-        SELECT * FROM Product
+        SELECT  p.*, s.ShopName FROM Product p JOIN Shop s ON s.ShopID = p.ShopID
         ${whereClause}
         LIMIT ? OFFSET ?;
     `;
@@ -228,10 +227,8 @@ const Products = {
   },
 
   getProductDetail: async (ProductID) => {
-    console.log("ProductID", ProductID);
-
     const result = await pool.query(
-      "SELECT * FROM Product WHERE ProductID = ? ",
+      "SELECT * FROM Product p JOIN Shop s ON s.ShopID = p.ShopID WHERE ProductID = ? ",
       [ProductID]
     );
     return result;
@@ -260,7 +257,7 @@ const Products = {
     }
 
     const queryDocs = `
-      SELECT * FROM Product p JOIN ProductFavorite pf ON pf.ProductID = p.ProductID 
+      SELECT * FROM Product p JOIN ProductFavorite pf ON pf.ProductID = p.ProductID JOIN Shop s ON s.ShopID = p.ShopID
       ${whereClause}
       LIMIT ? OFFSET ?;
   `;
@@ -439,7 +436,13 @@ const Products = {
         return result;
       }
     }
-  },  
+  },
+
+  checkUserCanComment: async(CustomerID, ProductID)=>{
+    const result = await pool.query('SELECT * FROM OrderDetail od JOIN Orders o ON o.OrderID = od.OrderID where o.CustomerID = ? AND ProductID  = ?',[CustomerID, ProductID]);
+    return result[0];
+  },
+   
 
 };
 module.exports = Products;
